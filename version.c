c66fd48b1ddfc9808d6916ec3754d0e8f08a1d3c
Chaos User <chaos@cc.gatech.edu>
2005-09-26 19:34:00 +0000

    normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@4644 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *CM_version = "CM Version 2.0.255 -- Wed Sep  7 14:45:42 EDT 2005\n";
+static char *CM_version = "CM Version 2.0.256 -- Mon Sep 26 15:34:00 EDT 2005\n";
 
 void CMprint_version(){
     printf("%s",CM_version);
