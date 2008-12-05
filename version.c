2bccc6919779beda297623f49b78a05d2bb50a2c
Chaos User <chaos@cc.gatech.edu>
2008-12-05 12:55:20 +0000

    normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@5036 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *EVPath_version = "EVPath Version 3.0.49 -- Thu Dec  4 13:26:15 EST 2008\n";
+static char *EVPath_version = "EVPath Version 3.0.50 -- Fri Dec  5 07:55:20 EST 2008\n";
 
 void EVprint_version(){
     printf("%s",EVPath_version);
