84cdf66def05cd404a696b31b3de3090a9ee823a
Greg Eisenhauer <eisen@cc.gatech.edu>
2011-11-07 00:30:25 +0000

    Bump version number to 3.1 because I changed the transport interfaces.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@10386 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *EVPath_version = "EVPath Version 3.0.140 rev. 10380  -- 2011-11-05 00:25:40 -0400 (Sat, 05 Nov 2011)\n";
+static char *EVPath_version = "EVPath Version 3.1.0 rev. 10380  -- 2011-11-05 00:25:40 -0400 (Sat, 05 Nov 2011)\n";
 
 #if defined (__INTEL_COMPILER)
 //  Allow extern declarations with no prior decl
