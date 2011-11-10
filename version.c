b8212110ce18b1683d8ae04e4586d12f3dac696c
Chaos User <chaos@cc.gatech.edu>
2011-11-10 07:00:16 +0000

    Normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@10404 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *EVPath_version = "EVPath Version 3.1.3 rev. 10394  -- 2011-11-08 14:29:44 -0500 (Tue, 08 Nov 2011)\n";
+static char *EVPath_version = "EVPath Version 3.1.4 rev. 10403  -- 2011-11-09 12:10:18 -0500 (Wed, 09 Nov 2011)\n";
 
 #if defined (__INTEL_COMPILER)
 //  Allow extern declarations with no prior decl
