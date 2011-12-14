60d6efd1d95f2a1b06c9d3adc094746eb3188a23
Greg Eisenhauer <eisen@cc.gatech.edu>
2011-12-14 15:44:20 +0000

    Repair segfault if format_list is NULL.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@10479 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- response.c
+++ response.c
@@ -438,7 +438,7 @@ INT_create_filter_action_spec(FMStructDescList format_list, char *function)
     int format_count = 0;
     int i;
     char *str;
-    while(format_list[format_count].format_name != NULL) format_count++;
+    while(format_list && (format_list[format_count].format_name != NULL)) format_count++;
     str = malloc(50);
     sprintf(str, "Filter Action   Format Count %d\n", format_count);
 
