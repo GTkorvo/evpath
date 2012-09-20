4cea8c5ecf4eccdce5dc645bb63a1f59774c6e91
Greg Eisenhauer <eisen@cc.gatech.edu>
2012-09-20 14:34:43 +0000

    Drop anon_multi_test.c into the repo so I can debug elsewhere.  Not running this test yet.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@11774 8ba6a8b9-845b-0410-94f0-974be2f5df47

---
+++ dfg_tests/anon_multi_test.c
@@ -0,0 +1,483 @@
+/*
+ *   Test queue manipulation techniques in EVPath queue stones.  In
+ *   particular, this test uses messages with anticipated types, as well as
+ *   testing manipulation of messages whose types were unanticipated (WRT
+ *   the queue stone handler) and which remain anonymous.  The idea is that
+ *   we'll dump a bunch of messages into a queue, then send 'command'
+ *   messages to cause certain manipulations of the queue, releasing some of
+ *   those queued messages.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+#include "ev_dfg.h"
+#include "test_support.h"
+
+static int status;
+static EVdfg test_dfg;
+
+
+typedef struct _rec_a {
+    int a_field;
+} rec_a, *rec_a_ptr;
+
+static FMField a_field_list[] =
+{
+    {"a_field", "integer",
+     sizeof(int), FMOffset(rec_a_ptr, a_field)},
+    {NULL, NULL, 0, 0}
+};
+
+static FMStructDescRec a_format_list[] =
+{
+    {"a_rec", a_field_list, sizeof(rec_a), NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+typedef struct _rec_b {
+    int b_field;
+} rec_b, *rec_b_ptr;
+
+static FMField b_field_list[] =
+{
+    {"b_field", "integer",
+     sizeof(int), FMOffset(rec_b_ptr, b_field)},
+    {NULL, NULL, 0, 0}
+};
+
+static FMStructDescRec b_format_list[] =
+{
+    {"b_rec", b_field_list, sizeof(rec_b), NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+typedef struct _rec_c {
+    int c_field;
+} rec_c, *rec_c_ptr;
+
+static FMField c_field_list[] =
+{
+    {"c_field", "integer",
+     sizeof(int), FMOffset(rec_c_ptr, c_field)},
+    {NULL, NULL, 0, 0}
+};
+
+static FMStructDescRec c_format_list[] =
+{
+    {"c_rec", c_field_list, sizeof(rec_c), NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+typedef struct _rec_command {
+    int command_field;
+} rec_command, *rec_command_ptr;
+
+static FMField command_field_list[] =
+{
+    {"command_field", "integer",
+     sizeof(int), FMOffset(rec_command_ptr, command_field)},
+    {NULL, NULL, 0, 0}
+};
+
+static FMStructDescRec command_format_list[] =
+{
+    {"command", command_field_list, sizeof(rec_command), NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+static FMStructDescRec anonymous_format_list[] =
+{
+    {"anonymous", NULL, 0, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+/*
+ *  'b' and 'c' are unknown to the queue stone
+ */
+static FMStructDescList queue_list[] = {a_format_list, command_format_list, anonymous_format_list, NULL};
+
+#define EVENT_COUNT 20
+#define COMMAND_COUNT 2
+
+static int repeat_count = EVENT_COUNT;
+
+static int sequence_num = 0;
+
+static
+void
+generate_a_record(rec_a_ptr event)
+{
+    event->a_field = ((int) lrand48() % 50) * 2 + 10000*++sequence_num;
+}
+
+static
+void
+generate_b_record(rec_b_ptr event)
+{
+    /* always odd */
+    event->b_field = ((int) lrand48() % 50) * 2 + 1 * 2 + 10000*++sequence_num;
+}
+
+static
+void
+generate_c_record(rec_c_ptr event)
+{
+    /* always odd */
+    event->c_field = ((int) lrand48() % 50) * 2 + 1 * 2 + 10000*++sequence_num;
+}
+
+static int expected_sequences[20]  = {10,2,3,0,0, 0,0,0,0,0, 0,0,0,0,0};
+static int received_sequences[EVENT_COUNT];
+
+static int message_count = 0;
+
+static void check_termination(int sequence)
+{
+    int i;
+    int result = 0;
+    received_sequences[message_count++] = sequence/10000;
+    if (message_count < EVENT_COUNT) return;
+    for (i=0; i < EVENT_COUNT; i++) {
+	if (expected_sequences[i] != received_sequences[i]) {
+	    printf("In slot %d, expected event %d, got %d\n", i, expected_sequences[i], received_sequences[i]);
+	    result=1;
+	}
+    }
+    EVdfg_shutdown(test_dfg, result);
+}
+
+static
+int
+a_output_handler(CManager cm, void *vevent, void *client_data, attr_list attrs)
+{
+    static int message_count = 0;
+    rec_a_ptr event = vevent;
+    (void)cm;
+    if (quiet <= 0) {
+	printf("In the handler, event data is :\n");
+	printf("	a_field = %d\n", event->a_field);
+	printf("Data was received with attributes : \n");
+	if (attrs) dump_attr_list(attrs);
+    }
+    check_termination(event->a_field);
+    return 0;
+}
+
+static
+int
+b_output_handler(CManager cm, void *vevent, void *client_data, attr_list attrs)
+{
+    static int message_count = 0;
+    rec_b_ptr event = vevent;
+    (void)cm;
+    if (quiet <= 0) {
+	printf("In the handler, event data is :\n");
+	printf("	b_field = %d\n", event->b_field);
+	printf("Data was received with attributes : \n");
+	if (attrs) dump_attr_list(attrs);
+    }
+    check_termination(event->b_field);
+    return 0;
+}
+
+static
+int
+c_output_handler(CManager cm, void *vevent, void *client_data, attr_list attrs)
+{
+    static int message_count = 0;
+    rec_c_ptr event = vevent;
+    (void)cm;
+    if (quiet <= 0) {
+	printf("In the handler, event data is :\n");
+	printf("	c_field = %d\n", event->c_field);
+	printf("Data was received with attributes : \n");
+	if (attrs) dump_attr_list(attrs);
+    }
+    check_termination(event->c_field);
+    return 0;
+}
+
+static char *trans = "{\n\
+    int found = 0;\n\
+    a_rec *a;\n\
+    printf(\"Event count is a_rec = %d, b_rec = %d, anon = %d\\n\", EVcount(0), EVcount(1), EVcount(-2));\n\
+    if (EVcount_command() > 0) {\n\
+	command *c = EVdata_command(0);\n\
+	if (c->command_field == 1) {\n\
+	    EVdiscard_and_submit_a_rec(0, EVcount_a_rec()-1);\n	\
+	    while(EVcount_a_rec()) {\n\
+	        printf(\"Submit and discard item 0 , count is %d\\n\", EVcount_a_rec());\n\
+	    EVdiscard_and_submit_a_rec(0, EVcount_a_rec()-1);\n	\
+	    }\n\
+	} else {\n\
+	        printf(\"Submit and discard item -2 , count is %d\\n\", EVcount_a_rec());\n\
+		EVdiscard_and_submit_anonymous(0,0);\n\
+	}\n\
+    }\n\
+printf(\"Exiting from handler\\n\");\n\
+}\0\0";
+
+static void
+data_free(void *event_data, void *client_data)
+{
+    (void) client_data;
+    free(event_data);
+}
+
+static int
+raw_handler(CManager cm, void *vevent, int len, void *client_data,
+	    attr_list attrs)
+{
+    static FFSContext c = NULL;
+    FFSTypeHandle f;
+    simple_rec incoming;
+    (void)len;
+    if (c == NULL) {
+	c = create_FFSContext();
+    }
+    
+    f = FFSTypeHandle_from_encode(c, vevent);
+    if (!FFShas_conversion(f)) {
+	establish_conversion(c, f, simple_format_list);
+    }
+    FFSdecode_to_buffer(c, vevent, &incoming);
+    return c_output_handler(cm, (void*) &incoming, client_data, attrs);
+}
+
+
+extern int
+be_test_master(int argc, char **argv)
+{
+    char *nodes[] = {"a", "b", "c", NULL};
+    CManager cm;
+    char *str_contact;
+    EVdfg_stone srca, srcb, srcc, srccommand, multiq, sink;
+    EVsource a_handle, b_handle, c_handle, command_handle;
+    char * q_action_spec;
+    int count, i;
+    int node_count = 3;
+    char *queue_node = "b";
+    char *terminal_node = "c";
+    atom_t index_atom;
+
+    argc; (void)argv;
+    cm = CManager_create();
+    if (argc == 1) {
+	if (!sscanf(argv[0], "%d", &node_count)) printf("arg \"%s\" not understood\n", argv[0]);
+	if ((node_count < 1) || (node_count > 3)) {
+	    printf("bad node count %d\n", node_count);
+	    node_count = 3;
+	}
+	nodes[node_count] = NULL;  /* slice off last few nodes */
+	switch (node_count) {
+	case 1:
+	    queue_node = nodes[0];
+	    terminal_node = nodes[0];
+	    break;
+	case 2:
+	    queue_node = nodes[1];
+	    terminal_node = nodes[1];
+	    break;
+	case 3:
+	    queue_node = nodes[1];
+	    terminal_node = nodes[2];
+	    break;
+	} 
+    }
+    CMlisten(cm);
+
+/*
+**  LOCAL DFG SUPPORT   Sources and sinks that might or might not be utilized.
+*/
+
+    a_handle = EVcreate_submit_handle_free(cm, -1, a_format_list,
+					   data_free, NULL);
+    b_handle = EVcreate_submit_handle_free(cm, -1, b_format_list,
+					   data_free, NULL);
+    c_handle = EVcreate_submit_handle_free(cm, -1, c_format_list,
+					   data_free, NULL);
+    command_handle = EVcreate_submit_handle_free(cm, -1, command_format_list,
+					   data_free, NULL);
+    EVdfg_register_source("a_source", a_handle);
+    EVdfg_register_source("b_source", b_handle);
+    EVdfg_register_source("c_source", c_handle);
+    EVdfg_register_source("command_source", command_handle);
+    EVdfg_register_sink_handler(cm, "a_output_handler", a_format_list,
+				(EVSimpleHandlerFunc) a_output_handler);
+    EVdfg_register_sink_handler(cm, "b_output_handler", b_format_list,
+				(EVSimpleHandlerFunc) b_output_handler);
+    EVdfg_register_sink_handler(cm, "c_output_handler", c_format_list,
+				(EVSimpleHandlerFunc) c_output_handler);
+    EVdfg_register_sink_handler(cm, "raw_output_handler", NULL,
+				(EVSimpleHandlerFunc) raw_handler);
+
+/*
+**  DFG CREATION
+*/
+    test_dfg = EVdfg_create(cm);
+    str_contact = EVdfg_get_contact_list(test_dfg);
+    EVdfg_register_node_list(test_dfg, &nodes[0]);
+
+    srca = EVdfg_create_source_stone(test_dfg, "a_source");
+    srcb = EVdfg_create_source_stone(test_dfg, "b_source");
+    srcc = EVdfg_create_source_stone(test_dfg, "c_source");
+    srccommand = EVdfg_create_source_stone(test_dfg, "command_source");
+    sink = EVdfg_create_sink_stone(test_dfg, "a_output_handler");
+    EVdfg_add_sink_action(sink, "b_output_handler");
+    EVdfg_add_sink_action(sink, "c_output_handler");
+    q_action_spec = create_multityped_action_spec(queue_list, trans);
+    multiq = EVdfg_create_stone(test_dfg, q_action_spec);
+    EVdfg_link_port(srca, 0, multiq);
+    EVdfg_link_port(srcb, 0, multiq);
+    EVdfg_link_port(srcc, 0, multiq);
+    EVdfg_link_port(srccommand, 0, multiq);
+    EVdfg_link_port(multiq, 0, sink);
+
+    EVdfg_assign_node(srca, "a");
+    EVdfg_assign_node(srcb, "a");
+    EVdfg_assign_node(srcc, "a");
+    EVdfg_assign_node(srccommand, "a");
+    EVdfg_assign_node(multiq, queue_node);
+    EVdfg_assign_node(sink, terminal_node);
+
+    EVdfg_realize(test_dfg);
+
+/* We're node 0 in the DFG */
+    EVdfg_join_dfg(test_dfg, nodes[0], str_contact);
+
+/* Fork the others */
+    test_fork_children(&nodes[0], str_contact);
+
+    if (EVdfg_ready_wait(test_dfg) != 1) {
+	/* dfg initialization failed! */
+	exit(1);
+    }
+
+    
+    index_atom = attr_atom_from_string("index_atom");
+    /* we know the sources are here, just do submits */
+    for (i=0; i < EVENT_COUNT + COMMAND_COUNT; i++) {
+	switch (i) {
+	case 5:
+	case 6:
+	case 9:
+	case 11:
+	case 12:
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 17:
+	case 18:
+	case 19:{
+	    attr_list attrs = create_attr_list();
+	    rec_a_ptr a = malloc(sizeof(*a));
+	    a->a_field = 1;
+	    generate_a_record(a);
+	    add_int_attr(attrs, index_atom, 1);
+	    
+	    if (quiet <=0) {printf("submitting a -> %d\n", a->a_field);}
+	    EVsubmit_general(a_handle, a, (EVFreeFunction)free, NULL);
+	    break;
+	}
+	case 0:
+	case 3:
+	case 4:
+	case 7: {
+	    attr_list attrs = create_attr_list();
+	    rec_b_ptr b = malloc(sizeof(*b));
+	    b->b_field = 2;
+	    generate_b_record(b);
+	    add_int_attr(attrs, index_atom, 2);
+	    
+	    if (quiet <=0) {printf("submitting b -> %d\n", b->b_field);}
+	    EVsubmit_general(b_handle, b, (EVFreeFunction)free, NULL);
+
+	    break;
+	}
+	case 1:
+	case 2:
+	case 8:	{
+	    attr_list attrs = create_attr_list();
+	    rec_c_ptr c = malloc(sizeof(*c));
+	    c->c_field = 3;
+	    generate_c_record(c);
+	    add_int_attr(attrs, index_atom, 3);
+	    
+	    if (quiet <=0) {printf("submitting c -> %d\n", c->c_field);}
+	    EVsubmit_general(c_handle, c, (EVFreeFunction)free, NULL);
+
+	    break;
+	}
+	case 21: {
+	    rec_command_ptr com = malloc(sizeof(*com));
+	    com->command_field = 2;
+	    EVsubmit(command_handle, com, NULL);
+	    break;
+	}
+	case 10: {
+	    rec_command_ptr com = malloc(sizeof(*com));
+	    com->command_field = 1;
+	    EVsubmit(command_handle, com, NULL);
+	    break;
+	}
+	}
+    }
+    CMsleep(cm, 1);
+
+    if (EVdfg_active_sink_count(test_dfg) == 0) {
+	EVdfg_ready_for_shutdown(test_dfg);
+    }
+
+    status = EVdfg_wait_for_shutdown(test_dfg);
+
+    wait_for_children(nodes);
+
+    CManager_close(cm);
+    return status;
+}
+
+
+extern int
+be_test_child(int argc, char **argv)
+{
+    CManager cm;
+    EVsource a_handle, b_handle;
+    int count, i;
+
+    cm = CManager_create();
+    if (argc != 3) {
+	printf("Child usage:  evtest  <nodename> <mastercontact>\n");
+	exit(1);
+    }
+    test_dfg = EVdfg_create(cm);
+
+    EVdfg_register_sink_handler(cm, "a_output_handler", a_format_list,
+				(EVSimpleHandlerFunc) a_output_handler);
+    EVdfg_register_sink_handler(cm, "b_output_handler", b_format_list,
+				(EVSimpleHandlerFunc) b_output_handler);
+    EVdfg_register_sink_handler(cm, "c_output_handler", c_format_list,
+				(EVSimpleHandlerFunc) c_output_handler);
+    a_handle = EVcreate_submit_handle_free(cm, -1, a_format_list,
+					   data_free, NULL);
+    b_handle = EVcreate_submit_handle_free(cm, -1, b_format_list,
+					   data_free, NULL);
+    EVdfg_register_source("a_source", a_handle);
+    EVdfg_register_source("b_source", b_handle);
+
+    EVdfg_join_dfg(test_dfg, argv[1], argv[2]);
+    EVdfg_ready_wait(test_dfg);
+
+    if (EVdfg_active_sink_count(test_dfg) == 0) {
+	EVdfg_ready_for_shutdown(test_dfg);
+    }
+
+    count = repeat_count;
+
+    /* sources are with the master, don't do submits here */
+
+    return EVdfg_wait_for_shutdown(test_dfg);
+}
