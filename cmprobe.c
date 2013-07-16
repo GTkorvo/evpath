3e3ce5f61807d3ab8cf29e72996af71f98a3f5bb
Greg Eisenhauer <eisen@cc.gatech.edu>
2013-07-16 03:46:00 +0000

    Trim useless code from this file.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@14977 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- cmprobe.c
+++ cmprobe.c
@@ -9,113 +9,6 @@
 #include <string.h>
 #include "evpath.h"
 #include <errno.h>
-#ifdef HAVE_WINDOWS_H
-#include <windows.h>
-#define drand48() (((double)rand())/((double)RAND_MAX))
-#define lrand48() rand()
-#define srand48(x)
-#endif
-
-typedef struct _complex_rec {
-    double r;
-    double i;
-} complex, *complex_ptr;
-
-typedef struct _nested_rec {
-    complex item;
-} nested, *nested_ptr;
-
-static FMField nested_field_list[] =
-{
-    {"item", "complex", sizeof(complex), FMOffset(nested_ptr, item)},
-    {NULL, NULL, 0, 0}
-};
-
-static FMField complex_field_list[] =
-{
-    {"r", "double", sizeof(double), FMOffset(complex_ptr, r)},
-    {"i", "double", sizeof(double), FMOffset(complex_ptr, i)},
-    {NULL, NULL, 0, 0}
-};
-
-typedef struct _simple_rec {
-    int integer_field;
-    short short_field;
-    long long_field;
-    nested nested_field;
-    double double_field;
-    char char_field;
-    int scan_sum;
-} simple_rec, *simple_rec_ptr;
-
-static FMField simple_field_list[] =
-{
-    {"integer_field", "integer",
-     sizeof(int), FMOffset(simple_rec_ptr, integer_field)},
-    {"short_field", "integer",
-     sizeof(short), FMOffset(simple_rec_ptr, short_field)},
-    {"long_field", "integer",
-     sizeof(long), FMOffset(simple_rec_ptr, long_field)},
-    {"nested_field", "nested",
-     sizeof(nested), FMOffset(simple_rec_ptr, nested_field)},
-    {"double_field", "float",
-     sizeof(double), FMOffset(simple_rec_ptr, double_field)},
-    {"char_field", "char",
-     sizeof(char), FMOffset(simple_rec_ptr, char_field)},
-    {"scan_sum", "integer",
-     sizeof(int), FMOffset(simple_rec_ptr, scan_sum)},
-    {NULL, NULL, 0, 0}
-};
-
-static FMStructDescRec simple_format_list[] =
-{
-    {"simple", simple_field_list, sizeof(simple_rec), NULL},
-    {"complex", complex_field_list, sizeof(complex), NULL},
-    {"nested", nested_field_list, sizeof(nested), NULL},
-    {NULL, NULL}
-};
-
-int quiet = 1;
-
-static
-void
-simple_handler(cm, conn, vevent, client_data, attrs)
-    CManager cm;
-    CMConnection conn;
-    void *vevent;
-    void *client_data;
-    attr_list attrs;
-{
-    simple_rec_ptr event = vevent;
-    long sum = 0, scan_sum = 0;
-    sum += event->integer_field % 100;
-    sum += event->short_field % 100;
-    sum += event->long_field % 100;
-    sum += ((int) (event->nested_field.item.r * 100.0)) % 100;
-    sum += ((int) (event->nested_field.item.i * 100.0)) % 100;
-    sum += ((int) (event->double_field * 100.0)) % 100;
-    sum += event->char_field;
-    sum = sum % 100;
-    scan_sum = event->scan_sum;
-    if (sum != scan_sum) {
-	printf("Received record checksum does not match. expected %d, got %d\n",
-	       (int) sum, (int) scan_sum);
-    }
-    if ((quiet <= 0) || (sum != scan_sum)) {
-	printf("In the handler, event data is :\n");
-	printf("	integer_field = %d\n", event->integer_field);
-	printf("	short_field = %d\n", event->short_field);
-	printf("	long_field = %ld\n", event->long_field);
-	printf("	double_field = %g\n", event->double_field);
-	printf("	char_field = %c\n", event->char_field);
-    }
-    if (client_data != NULL) {
-	int tmp = *((int *) client_data);
-	if (tmp > 0) {
-	    *((int *) client_data) = tmp - 1;
-	}
-    }
-}
 
 int
 main(argc, argv)
@@ -127,7 +20,6 @@ main(argc, argv)
     CMFormat format;
     static int atom_init = 0;
 
-    srand48(getpid());
     cm = CManager_create();
     (void) CMfork_comm_thread(cm);
 
@@ -157,8 +49,6 @@ main(argc, argv)
 	CMlisten_specific(cm, listen_list);
 	contact_list = CMget_contact_list(cm);
 	printf("Contact list \"%s\"\n", attr_list_to_string(contact_list));
-	format = CMregister_format(cm, simple_format_list);
-	CMregister_handler(format, simple_handler, NULL);
 	CMsleep(cm, 1200);
     } else {
 	int size = 100;
