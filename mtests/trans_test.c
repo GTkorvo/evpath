91778d38d070c4ead509dae0684802a1227922e7
Greg Eisenhauer <eisen@cc.gatech.edu>
2013-07-19 14:08:13 +0000

    Implement code to take receive buffers.  -take_receive_buffer 0/1
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@15003 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- mtests/trans_test.c
+++ mtests/trans_test.c
@@ -15,24 +15,37 @@ static atom_t CM_TRANS_TEST_VECS = 4;
 static atom_t CM_TRANS_TEST_VERBOSE = -1;
 static atom_t CM_TRANS_TEST_REPEAT = 10;
 static atom_t CM_TRANS_TEST_REUSE_WRITE_BUFFER = -1;
+static atom_t CM_TRANS_TEST_TAKE_RECEIVE_BUFFER = -1;
 static atom_t CM_TRANSPORT = -1;
 static atom_t CM_TRANS_TEST_RECEIVED_COUNT = -1;
+static atom_t CM_TRANS_TEST_TAKEN_CORRUPT = -1;
 
 static int vec_count = 4;
 static int size = 10240;
 static int msg_count = 10;
+static int reuse_write = 1;
 
 static int received_count = 0;
+static int taken_corrupt = 0;
 static int expected_count = -1;
 static int write_size = -1;
 static int verbose = 0;
+static int take = 0;
 static int size_error = 0;
 static int global_exit_condition = -1;
 static attr_list global_test_result = NULL;
 
+typedef struct _buf_list {
+    int checksum;
+    long length;
+    void *buffer;
+} *buf_list;
+
 attr_list
 trans_test_upcall(CManager cm, void *buffer, long length, int type, attr_list list)
 {
+    static buf_list buffer_list = NULL;
+    static int buffer_count = 0;
     switch(type) {
     case 0:
 	/* test init */
@@ -46,6 +59,7 @@ trans_test_upcall(CManager cm, void *buffer, long length, int type, attr_list li
 	    get_int_attr(list, CM_TRANS_TEST_REPEAT, &expected_count);
 	    get_int_attr(list, CM_TRANS_TEST_SIZE, &write_size);
 	    get_int_attr(list, CM_TRANS_TEST_VERBOSE, &verbose);
+	    get_int_attr(list, CM_TRANS_TEST_TAKE_RECEIVE_BUFFER, &take);
 	}
 	return NULL;
 	break;
@@ -53,7 +67,7 @@ trans_test_upcall(CManager cm, void *buffer, long length, int type, attr_list li
 	/* body message */
 	if (verbose) printf("Body message %d received, length %ld\n", *(int*)buffer, length);
 	if (length != (write_size - 12 /* test protocol swallows a little as header */)) {
-	    if (verbose) printf("Error in body delivery size, expected %d, got %ld\n", write_size - 12, length);
+	    printf("Error in body delivery size, expected %d, got %ld\n", write_size - 12, length);
 	    size_error++;
 	}
 	if (*(int*)buffer != received_count) {
@@ -63,13 +77,50 @@ trans_test_upcall(CManager cm, void *buffer, long length, int type, attr_list li
 		warned++;
 	    }
 	}
+	if (take) {
+	    int sum = 0;
+	    int i;
+	    if (!buffer_list) {
+		buffer_list = malloc(sizeof(buffer_list[0]));
+	    } else {
+		buffer_list = realloc(buffer_list, (buffer_count+1)*sizeof(buffer_list[0]));
+	    }
+	    for(i=0; i < length/sizeof(int); i++) {
+		sum += ((int*)buffer)[i];
+	    }
+	    buffer_list[buffer_count].buffer = buffer;
+	    buffer_list[buffer_count].length = length;
+	    buffer_list[buffer_count].checksum = sum;
+	    CMtake_buffer(cm, buffer);
+	    buffer_count++;
+	}
 	received_count++;
 	return NULL;
 	break;
     case 2: {
 	/* test finalize */
 	attr_list ret = create_attr_list();
+	int buf;
+	for (buf = 0; buf < buffer_count; buf++) {
+	    int sum = 0;
+	    int i;
+	    for(i=0; i < buffer_list[buf].length/sizeof(int); i++) {
+		sum += ((int*)buffer_list[buf].buffer)[i];
+	    }
+	    if (buffer_list[buf].checksum != sum) {
+		printf("taken data corrupted, calc checksum %d, stored %d, first entry %d\n", sum, buffer_list[buf].checksum, ((int*)buffer_list[buf].buffer)[0]);
+		taken_corrupt++;
+	    }
+	    CMreturn_buffer(cm, buffer_list[buf].buffer);
+	}
+	if (buffer_list) {
+	    free(buffer_list);
+	    buffer_list = NULL;
+	}
 	set_int_attr(ret, CM_TRANS_TEST_RECEIVED_COUNT, received_count);
+	if (taken_corrupt) {
+	    set_int_attr(ret, CM_TRANS_TEST_TAKEN_CORRUPT, taken_corrupt);
+	}
 	global_test_result = ret;
 	if (global_exit_condition != -1) {
 	    CMCondition_signal(cm, global_exit_condition);
@@ -137,22 +188,39 @@ main(argc, argv)
 	} else if (strcmp(&argv[1][1], "v") == 0) {
 	    verbose++;
 	} else if (strcmp(&argv[1][1], "vectors") == 0) {
-	    if (!argv[2] || (sscanf("%d", argv[2], &vec_count) != 1)) {
+	    if (!argv[2] || (sscanf(argv[2], "%d", &vec_count) != 1)) {
 		printf("Bad -vectors argument \"%s\"\n", argv[2]);
 		usage();
 	    }
+	    subproc_args[cur_subproc_arg++] = strdup(argv[2]);
 	    argv++; argc--;
 	} else if (strcmp(&argv[1][1], "size") == 0) {
-	    if (!argv[2] || (sscanf("%d", argv[2], &size) != 1)) {
+	    if (!argv[2] || (sscanf(argv[2], "%d", &size) != 1)) {
 		printf("Bad -size argument \"%s\"\n", argv[2]);
 		usage();
 	    }
+	    subproc_args[cur_subproc_arg++] = strdup(argv[2]);
 	    argv++; argc--;
 	} else if (strcmp(&argv[1][1], "msg_count") == 0) {
-	    if (!argv[2] || (sscanf("%d", argv[2], &msg_count) != 1)) {
+	    if (!argv[2] || (sscanf(argv[2], "%d", &msg_count) != 1)) {
 		printf("Bad -msg_count argument \"%s\"\n", argv[2]);
 		usage();
 	    }
+	    subproc_args[cur_subproc_arg++] = strdup(argv[2]);
+	    argv++; argc--;
+	} else if (strcmp(&argv[1][1], "reuse_write_buffers") == 0) {
+	    if (!argv[2] || (sscanf(argv[2], "%d", &reuse_write) != 1)) {
+		printf("Bad -reuse_write argument \"%s\"\n", argv[2]);
+		usage();
+	    }
+	    subproc_args[cur_subproc_arg++] = strdup(argv[2]);
+	    argv++; argc--;
+	} else if (strcmp(&argv[1][1], "take_receive_buffer") == 0) {
+	    if (!argv[2] || (sscanf(argv[2], "%d", &take) != 1)) {
+		printf("Bad -take_receive_buffer argument \"%s\"\n", argv[2]);
+		usage();
+	    }
+	    subproc_args[cur_subproc_arg++] = strdup(argv[2]);
 	    argv++; argc--;
 	} else if (strcmp(&argv[1][1], "n") == 0) {
 	    start_subprocess = 0;
@@ -172,7 +240,9 @@ main(argc, argv)
 	CM_TRANS_TEST_VERBOSE = attr_atom_from_string("CM_TRANS_TEST_VERBOSE");
 	CM_TRANS_TEST_REPEAT = attr_atom_from_string("CM_TRANS_TEST_REPEAT");
 	CM_TRANS_TEST_REUSE_WRITE_BUFFER = attr_atom_from_string("CM_TRANS_TEST_REUSE_WRITE_BUFFER");
+	CM_TRANS_TEST_TAKE_RECEIVE_BUFFER = attr_atom_from_string("CM_TRANS_TEST_TAKE_RECEIVE_BUFFER");
 	CM_TRANS_TEST_RECEIVED_COUNT = attr_atom_from_string("CM_TRANS_TEST_RECEIVED_COUNT");
+	CM_TRANS_TEST_TAKEN_CORRUPT = attr_atom_from_string("CM_TRANS_TEST_TAKEN_CORRUPT");
 	CM_TRANSPORT = attr_atom_from_string("CM_TRANSPORT");
 	atom_init++;
     }
@@ -231,6 +301,9 @@ main(argc, argv)
 	add_int_attr(test_list, CM_TRANS_TEST_SIZE, size); 
 	add_int_attr(test_list, CM_TRANS_TEST_VECS, vec_count); 
 	add_int_attr(test_list, CM_TRANS_TEST_REPEAT, msg_count); 
+	add_int_attr(test_list, CM_TRANS_TEST_REUSE_WRITE_BUFFER, reuse_write); 
+	add_int_attr(test_list, CM_TRANS_TEST_TAKE_RECEIVE_BUFFER, take); 
+	add_int_attr(test_list, CM_TRANS_TEST_VERBOSE, verbose);
 		
 	result = CMtest_transport(conn, test_list);
     }
