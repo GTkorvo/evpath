62cd0a20be086b5bf682af6d770838985bc5c8b0
Greg Eisenhauer <eisen@cc.gatech.edu>
2013-07-29 20:06:13 +0000

    Clean up multiple 'until no change' loops.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@15065 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- mtests/transport_test.pl
+++ mtests/transport_test.pl
@@ -16,54 +16,53 @@ die "No test spec file found\n" unless $return;
 
 print "Transport list is @transport_list\n";
 $change = 1;
-Dumper(\@test_set);
-while ($change) {
+
+while ( my ($key, $value) = each(%test_set) ) {
+    print "$key => ".@{$value}." ". Dumper($value)."\n";
+    if (@{$value} == 0) {
+	$test_set{$key}=undef;
+	$test_set{$key} = [];
+	$ret = push @{$test_set{$key}}, $value;
+    }
     $change = 1;
-    while ( my ($key, $value) = each(%test_set) ) {
-        print "$key => ".@{$value}." ". Dumper($value)."\n";
-	if (@{$value} == 0) {
-	  $test_set{$key}=undef;
-	  $test_set{$key} = [];
-	  $ret = push @{$test_set{$key}}, $value;
-	}
-	while($change) {
-	  $change = 0;
-	  $value = $test_set{$key};
-	  my $i = 0;
-	  while( $i < scalar @{$value}) {
+    while($change) {
+	$change = 0;
+	$value = $test_set{$key};
+	my $i = 0;
+	while( $i < scalar @{$value}) {
 	    $this_line = @{$value}[$i];
 	    @components = split(/ /, $this_line);
 	    $prefix = "";
 	    if ($components[0] eq "") {shift(@components);}
 	    component_loop : while ($element = shift(@components)) {
-	      if ($element =~ /.*:.*/) {
-		$prefix = "$prefix $element";
-	      } else {
-		# if the element has no colon
-		if (! exists $macro{$element}) {
-		  printf("Macro $element not defined\n");
-		  next;
+		if ($element =~ /.*:.*/) {
+		    $prefix = "$prefix $element";
+		} else {
+		    # if the element has no colon
+		    if (! exists $macro{$element}) {
+			printf("Macro $element not defined\n");
+			next;
+		    }
+		    $postfix = join(' ', @components);
+		    my @new_elements = ();
+		    for my $j (0 .. @{$macro{$element}}-1) {
+			my $string = "$prefix $macro{$element}[$j] $postfix";
+			$string =~ s/^\s+//;
+			$string =~ s/\s+$//; 
+			push @new_elements, $string;
+		    }
+		    splice(@{$value}, $i, 1, @new_elements);
+		    $i += scalar(@new_elements);
+		    $change = 1;
+		    $test_set{$key} = $value;
+		    last component_loop;
 		}
-		$postfix = join(' ', @components);
-		my @new_elements = ();
-		for my $j (0 .. @{$macro{$element}}-1) {
-		  my $string = "$prefix $macro{$element}[$j] $postfix";
-		  $string =~ s/^\s+//;
-		  $string =~ s/\s+$//; 
-		  push @new_elements, $string;
-		}
-		splice(@{$value}, $i, 1, @new_elements);
-		$i += scalar(@new_elements);
-		$change = 1;
-		$test_set{$key} = $value;
-		last component_loop;
-	      }
 	    }
 	    $i++;
-	  }
-	}	
-        # $val's used here
+	}
     }
+    print("End of change loop, key $key\n");
+    
 }
 
 
@@ -76,9 +75,9 @@ while ( my ($transport, $value) = each(%test_set) ) {
   foreach (@{$value}) {
     $_ =~ s/(\w+):(\w+)/-\1 \2/g;
     $_ = "./trans_test -transport $transport $_";
-    print "Would run $_ \n";
+    print "Running: $_ \n";
     my $output = `$_`;
-    my $my_exit_code = ($?>>8);
+    my $my_exit_code = $?;
     if ($my_exit_code != 0) {
       print ("Error while running $_\n");
       print ("Output was:\n");
