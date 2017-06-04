b7135bd2b20ea5442c6f5504ed69ea4cba3084ea
Greg Eisenhauer <eisen@cc.gatech.edu>
2017-06-04 02:56:34 +0000

    Doooh.  Was testing the wrong input parameter for having the high bit set.
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/evpath/trunk@26924 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- evp.c
+++ evp.c
@@ -96,7 +96,7 @@ extern void
 INT_CMadd_stone_to_global_lookup(CManager cm, int stone_num, int global_stone_num)
 {
     event_path_data evp = cm->evp;
-    if ((stone_num & 0x80000000) != 0x80000000) {
+    if ((global_stone_num & 0x80000000) != 0x80000000) {
         fprintf(stderr, "Global stone num must have 32nd bit set.  Value provided was %x\n", global_stone_num);
         fprintf(stderr, "Ignoring call to CMadd_stone_to_global_lookup for stone %d\n", stone_num);
         return;
