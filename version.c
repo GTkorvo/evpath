11be50d0e4df1723bdb855a747cf72d3d965bade
Greg Eisenhauer <eisen@cc.gatech.edu>
2017-12-04 16:21:57 -0500

    Update version

--- version.c
+++ version.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "config.h"
 
-static char *EVPath_version = "EVPath Version 4.2.3 -- Thu Nov  2 09:59:50 EDT 2017\n";
+static char *EVPath_version = "EVPath Version 4.2.4 -- Mon Dec  4 16:21:33 EST 2017\n";
 
 #if defined (__INTEL_COMPILER)
 //  Allow extern declarations with no prior decl
