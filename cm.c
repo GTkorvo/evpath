af9cd717c687f93778984d104c4e3bd6941d1304
Greg Eisenhauer <eisen@cc.gatech.edu>
2023-07-31 18:33:57 +0100

    Windows DLL handling tweaks

--- cm.c
+++ cm.c
@@ -3773,7 +3773,11 @@ CM_init_select(CMControlList cl, CManager cm)
     lt_dladdsearchdir(EVPATH_MODULE_BUILD_DIR);
     lt_dladdsearchdir(EVPATH_MODULE_INSTALL_DIR);
     libname = malloc(strlen("lib" CM_LIBRARY_PREFIX "cm") + strlen(select_module) + strlen(MODULE_EXT) + 1);
+#ifndef HAVE_WINDOWS_H
     strcpy(libname, "lib" CM_LIBRARY_PREFIX "cm");
+#else
+    strcpy(libname, CM_LIBRARY_PREFIX "cm");
+#endif
     strcat(libname, select_module);
     strcat(libname, MODULE_EXT);
     handle = CMdlopen(cm->CMTrace_file, libname, 0);
